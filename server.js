b5004a03b42991b8ccbab6df77b224516473edd4
Nathanael White <narhanaelw99@gmail.com>
2021-05-29 11:30:45 -0500

    added app.use to server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 // const { notes } = require('./data/db');
 const apiRoutes = require('./routes/apiRoutes/noteRoutes');
 // const htmlRoutes = require('./routes/htmlRoutes');
+const htmlRoutes = require('./routes/htmlRoutes/index');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -15,7 +16,7 @@ app.use(express.json());
 
 app.use('/api', apiRoutes);
 // app.use for htmlRoutes
-
+app.use('/', htmlRoutes)
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}`);
 });
